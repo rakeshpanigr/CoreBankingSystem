e8dfea6ac6af776f147dae7efa61fb96f0381bd5
Aliva Tripathy(UST <305945@ust.com>
2026-04-09 22:05:58 +0530

    Customer and address model creation

---
+++ src/org/li/banking/model/Customer.java
@@ -0,0 +1,64 @@
+package org.li.banking.model;
+
+import java.util.Objects;
+
+public class Customer {
+    private String name;
+    private String email;
+    private String phoneNo;
+    private String pan;
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(phoneNo, customer.phoneNo) && Objects.equals(pan, customer.pan);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, phoneNo, pan);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNo=" + phoneNo +
+                ", pan='" + pan + '\'' +
+                '}';
+    }
+}
--- src/org/li/banking/model/Model.java
+++
@@ -1,4 +0,0 @@
-package org.li.banking.model;
-
-public class Model {
-}
